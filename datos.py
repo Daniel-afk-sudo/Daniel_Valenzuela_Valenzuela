983c536581f17e5473f33fb31831223eda393646
Daniel Valenzuela Valenzuela <daneilvalenzuela26316@gmail.com>
2024-09-03 11:46:49 -0400

    Añadido archivo de código fuente datos.py

---
+++ datos.py
@@ -0,0 +1,5 @@
+productos = [
+    {"nombre": "Camiseta", "precio": 20.0, "stock": 50},
+    {"nombre": "Zapatillas", "precio": 60.0, "stock": 30},
+    {"nombre": "Gorra", "precio": 15.0, "stock": 40}
+]
